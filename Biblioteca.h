da5d6edee695e5939f0df50a8e5069722ee511cf
Amanda <amanda@aduanaweb.com.br>
2016-09-20 14:27:24 -0300

    ultimas atualizacoes

--- Biblioteca.h
+++ Biblioteca.h
@@ -3,7 +3,7 @@
 typedef struct trainer {
     int chave;
     int tPokeballs;
-    int tPokedex; // fazer uma lista (pokémon, cp)
+    int tPokedex; // fazer uma lista (pok�mon, cp)
     int sumScore;
 } tTrainer;
 
@@ -26,8 +26,9 @@ typedef struct pos {
 //.
 
 /* ---- Cabecalho de Funcoes */
-// Funções de Listas
-void criaLista(lTrainer *lista);
+// Funcoes de Listas
+void criaListaTrainer(lTrainer *lista);
+void insereTrainer(tTrainer x, lTrainer *lista);
 
 // Funcoes de Jogo
 void explore();
--- Funcoes.c
+++ Funcoes.c
@@ -3,11 +3,19 @@
 #include "Biblioteca.h"
 
 /* ---- Funcoes de Listas */
-//
-void criaLista(lTrainer *lista) {
+// cria uma lista de jogadores
+void criaListaTrainer(lTrainer *lista) {
  lista->primeiro = (tCelula *)malloc(sizeof(tCelula));
  lista->ultimo = lista->primeiro;
  lista->primeiro->prox = NULL;
+}
+
+// insere novo jogador em uma lista
+void insereTrainer(tTrainer x, lTrainer *lista) {
+ lista->ultimo->prox  = (tCelula *)malloc(sizeof(tCelula));
+ lista->ultimo = lista->ultimo->prox;
+ lista->ultimo->treinador = x;
+ lista->ultimo->prox = NULL;
 }
 //.
 
